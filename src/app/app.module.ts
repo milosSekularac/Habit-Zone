9748c4cdd560a562f18b7ce78809b8c2ec08a9d8
milosSekularac <108265415+milosSekularac@users.noreply.github.com>
2022-12-24 15:47:42 +0100

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopComponent } from './top/top.component';
+import { HabitsComponent } from './habits/habits.component';
+import { DailiesComponent } from './dailies/dailies.component';
+import { ToDoComponent } from './to-do/to-do.component';
+import { RewardsComponent } from './rewards/rewards.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopComponent,
+    HabitsComponent,
+    DailiesComponent,
+    ToDoComponent,
+    RewardsComponent,
+    TopComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
